cffd78854a3afcdddce2b92cb26750814d10f4b3
SI-Guillaume <Guillaume.Clavier@groupesurplus.fr>
2026-05-07 17:14:07 +0200

    Synchro dossier PDA entre Gaillac et l'Union
    Synchro manuel du dossier PDA entre Gaillac et l'Union
    
    A l'arrivé d'un nouveau APK Atemo, j'utilise ce dossier pour la maintenance des PDA et leur installation
    
    Robocopy du script :
    set "SRC=\\srvfile05\COMMUN\PDA"
    set "DST=\\srvad05\PDA"

---
+++ Synchro APK ATEMO.bat
@@ -0,0 +1,11 @@
+﻿@echo off
+setlocal EnableDelayedExpansion
+
+set "SRC=\\srvfile05\COMMUN\PDA"
+set "DST=\\srvad05\PDA"
+
+echo Synchronisation de %SRC% vers %DST%
+robocopy "%SRC%" "%DST%" /MIR /Z /W:5 /R:3 /TEE /LOG:"%~dp0robocopy_log.txt"
+
+echo Copie terminer.
+pause
